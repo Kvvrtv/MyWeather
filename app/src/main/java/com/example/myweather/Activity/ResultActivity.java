211470af3683c44ed56e3f0e672dfb1797c881ad
Kvvrtv <kvvrtv@users.noreply.github.com>
2023-03-07 15:36:52 +0100

    zweite View

--- app/src/main/java/com/example/myweather/Activity/ResultActivity.java
+++ app/src/main/java/com/example/myweather/Activity/ResultActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class BaseActivity extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
